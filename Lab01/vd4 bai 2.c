ab5f5a470cd75646226bfe1da8e85c77d92d9514
ZeusPhucNguyen <73330508+ZeusPhucNguyen@users.noreply.github.com>
2020-10-23 14:17:10 +0700

    Add files via upload

---
+++ Lab01/vd4 bai 2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int x,y;
+	x=5;
+	y=2;
+	printf("the intergers are :%d & %d\n",x,y);
+	printf("the adition gives :%d\n",x+y);
+	printf("the subtraction gives :%d\n",x-y);
+	printf("the multiplication gives: %d\n",x*y);
+	printf("the division gives:%d\n",x/y);
+	printf("the modulus gives:%d\n",x%y);
+	getchar();
+	
+}
